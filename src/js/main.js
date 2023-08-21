db58b7a85cc638c3a36efaa5004c338c8ebef06f
KostaSenatov <kostyasenatov@gmail.com>
2023-08-21 12:25:33 +0400

    .

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 //= ./libs/jquery-3.7.0.min.js
 //= ./libs/swiper-bundle.min.js
+//= ./libs/aos.js
 
 
 //= ./partials/menu-switch.js
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+	$(document).on('mousemove', function(e){
+		$('#circle-cursor').css({
+			left:  e.pageX,
+			top:   e.pageY
+		});
+	});
+
+
+/* ---------------------появление элементов при прокрутке--------------------------- */
+	function onEntry(entry) {
+		entry.forEach(change => {
+			if (change.isIntersecting) {
+				change.target.classList.add('el-shown');
+			}
+		});
+	}
+	let options = {
+		threshold: [0.5] };
+	let observer = new IntersectionObserver(onEntry, options);
+	let elements = document.querySelectorAll('.el-hidden');
+	for (let elm of elements) {
+		observer.observe(elm);
+	}
+
+
 	$('.header__burger').click(function() {
 		$('.header__burger').toggleClass('js-active');
 		$('.menu-header').toggleClass('js-active');
@@ -35,6 +60,24 @@ $(document).ready(function() {
 	})
 	
 
+	
+
+
+/* 	var headers = $('.general-title');
+
+	headers.each(function() {
+		var text = $(this).text();
+
+		var lines = text.split('\n');
+
+		$(this).empty();
+		lines.each(function(i, line) {
+			$(this).append($('<span>').text(line));
+		});
+	}); */
+
+
+
 
 
 	$('.click').on('click', function() {
@@ -55,60 +98,59 @@ $(document).ready(function() {
 
 
 
-if ($(window).innerWidth() > 768) {
-	
-	$(function(){	
-		var column = 0;
-		$('.aivsman-body .aivsman-body__same-height1').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height2').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height3').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height4').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height5').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height6').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height7').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-		$('.aivsman-body .aivsman-body__same-height8').each(function(){
-			h = $(this).height();
-			if (h > column) {
-				column = h;
-			}
-		}).height(column);
-	});
-}
+	if ($(window).innerWidth() > 768) {
+		$(function(){	
+			var column = 0;
+			$('.aivsman-body .aivsman-body__same-height1').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height2').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height3').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height4').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height5').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height6').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height7').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+			$('.aivsman-body .aivsman-body__same-height8').each(function(){
+				h = $(this).height();
+				if (h > column) {
+					column = h;
+				}
+			}).height(column);
+		});
+	}
 
 	setTimeout(function () {
 		var aivsman_switcher = $('.aivsman-switcher').innerHeight();
@@ -137,12 +179,17 @@ if ($(window).innerWidth() > 768) {
 
 
 		$(window).scroll(function() {
+			if ($(window).scrollTop() > $(window).innerHeight()) {
+				$('.btn-up').addClass('js-active');
+			}else {
+				$('.btn-up').removeClass('js-active');
+			}
+			
+			
 			if ($('.aivsman').offset().top + 100 < $(window).scrollTop()) {
 				$('.aivsman-switch-content__container-titles').addClass('js-active');
-				$('.aivsman-switcher').addClass('js-active');
 			}else {
 				$('.aivsman-switch-content__container-titles').removeClass('js-active');
-				$('.aivsman-switcher').removeClass('js-active');
 			}
 
 
@@ -162,133 +209,167 @@ if ($(window).innerWidth() > 768) {
 		})
 
 
-		
+
+
+		/* $('.aivsman-body__subtitle.general-title').each(function() {
+			var text = $(this).text();
+			wrappedText = '<p class="general-title-inner colored-text">' + text + '</p>';
+			$(this).html(wrappedText);
+		}); */
+		/* $('.aivsman-body__subtitle.general-title').each(function() {
+			var text = $(this).text();
+			var wrappedText = '';
+			for (var i = 0; i < text.length; i++) {
+				var letter = text[i];
+				wrappedText += '<p class="title-inner colored-text">' + letter + '</p>';
+			}
+			$(this).html(wrappedText);
+		});
+		$('.aivsman-body__subtitle.general-title .title-outer').each(function() {
+			var text = $(this).text();
+			var wrappedText = '';
+			for (var i = 0; i < text.length; i++) {
+				var letter = text[i];
+				wrappedText += '<p class="title-inner colored-text">' + letter + '</p>';
+			}
+			$(this).html(wrappedText);
+		});
+		$('.aivsman-body__subtitle.general-title .title-outer .title-inner').each(function() {
+			if ($(this).innerhtml() = " ") {
+				
+			}
+		}); */
+
+
+
+
+
 	}, 500);
 
 
 
 
 
-const hero_images = gsap.timeline();
+	const hero_images = gsap.timeline();
 
-hero_images.fromTo('.hero__image-parallax1', 1, {y: -50, transform: 'scale(1)'}, {y:50, transform: 'scale(1)'}, 0)
-hero_images.fromTo('.hero__image-parallax2', 1, {y: -50, transform: 'scale(0.7)'}, {y:50, transform: 'scale(1.3)'}, 0)
-hero_images.fromTo('.hero__image-parallax3', 1, {y: 150, transform: 'scale(0.7)'}, {y:-50, transform: 'scale(1.3)'}, 0)
-hero_images.fromTo('.hero__image-parallax4', 1, {y: 50}, {y:-50}, 0)
+	hero_images.fromTo('.hero__image-parallax1', 1, {y: -50, transform: 'scale(1)'}, {y:50, transform: 'scale(1)'}, 0)
+	hero_images.fromTo('.hero__image-parallax2', 1, {y: -50, transform: 'scale(0.7)'}, {y:50, transform: 'scale(1.3)'}, 0)
+	hero_images.fromTo('.hero__image-parallax3', 1, {y: 150, transform: 'scale(0.7)'}, {y:-50, transform: 'scale(1.5)'}, 0)
+	hero_images.fromTo('.hero__image-parallax4', 1, {y: 50}, {y:-50}, 0)
 
-ScrollTrigger.create({
-	animation: hero_images,
-	trigger: '.hero',
-	scrub: 2,
-})
-
-
-
-
-const body__sec2 = gsap.timeline();
-
-body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
-body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
-
-ScrollTrigger.create({
-	animation: body__sec2,
-	trigger: '.aivsman-body__sec2',
-	scrub: 2,
-})
+	ScrollTrigger.create({
+		animation: hero_images,
+		trigger: '.hero',
+		scrub: 2,
+	})
 
 
 
 
-const body__sec5 = gsap.timeline();
+	const body__sec2 = gsap.timeline();
 
-body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
-body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+	body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
+	body__sec2.fromTo('.aivsman-body__sec2 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
 
-ScrollTrigger.create({
-	animation: body__sec5,
-	trigger: '.aivsman-body__sec5',
-	scrub: 3,
-})
-
-
-const body__sec6 = gsap.timeline();
-
-body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax2', 1, {y: 50}, {y:-50}, 0)
-body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
-
-ScrollTrigger.create({
-	animation: body__sec6,
-	trigger: '.aivsman-body__sec6',
-	scrub: 1,
-})
-
-
-const body__sec7 = gsap.timeline();
-
-body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
-body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
-
-ScrollTrigger.create({
-	animation: body__sec7,
-	trigger: '.aivsman-body__sec7',
-	scrub: 2,
-})
-const body__sec22 = gsap.timeline();
-
-body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
-body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
-
-ScrollTrigger.create({
-	animation: body__sec22,
-	trigger: '.aivsman-body__sec22',
-	scrub: 2,
-})
+	ScrollTrigger.create({
+		animation: body__sec2,
+		trigger: '.aivsman-body__sec2',
+		scrub: 2,
+	})
 
 
 
 
-const body__sec55 = gsap.timeline();
+	const body__sec5 = gsap.timeline();
 
-body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
-body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+	body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
+	body__sec5.fromTo('.aivsman-body__sec5 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
 
-ScrollTrigger.create({
-	animation: body__sec55,
-	trigger: '.aivsman-body__sec55',
-	scrub: 3,
-})
-
-
-const body__sec66 = gsap.timeline();
-
-body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax2', 1, {y: 50}, {y:-50}, 0)
-body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
-
-ScrollTrigger.create({
-	animation: body__sec66,
-	trigger: '.aivsman-body__sec66',
-	scrub: 1,
-})
+	ScrollTrigger.create({
+		animation: body__sec5,
+		trigger: '.aivsman-body__sec5',
+		scrub: 3,
+	})
 
 
-const body__sec77 = gsap.timeline();
+	const body__sec6 = gsap.timeline();
 
-body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
-body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax2', 1, {y: 100}, {y:-100}, 0)
-body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+	body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax2', 1, {y: 50}, {y:-50}, 0)
+	body__sec6.fromTo('.aivsman-body__sec6 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
 
-ScrollTrigger.create({
-	animation: body__sec77,
-	trigger: '.aivsman-body__sec77',
-	scrub: 2,
-})
+	ScrollTrigger.create({
+		animation: body__sec6,
+		trigger: '.aivsman-body__sec6',
+		scrub: 1,
+	})
+
+
+	const body__sec7 = gsap.timeline();
+
+	body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
+	body__sec7.fromTo('.aivsman-body__sec7 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+
+	ScrollTrigger.create({
+		animation: body__sec7,
+		trigger: '.aivsman-body__sec7',
+		scrub: 2,
+	})
+	const body__sec22 = gsap.timeline();
+
+	body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
+	body__sec22.fromTo('.aivsman-body__sec22 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+
+	ScrollTrigger.create({
+		animation: body__sec22,
+		trigger: '.aivsman-body__sec22',
+		scrub: 2,
+	})
+
+
+
+
+	const body__sec55 = gsap.timeline();
+
+	body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax2', 1, {y: -100}, {y:100}, 0)
+	body__sec55.fromTo('.aivsman-body__sec55 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+
+	ScrollTrigger.create({
+		animation: body__sec55,
+		trigger: '.aivsman-body__sec55',
+		scrub: 3,
+	})
+
+
+	const body__sec66 = gsap.timeline();
+
+	body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax2', 1, {y: 50}, {y:-50}, 0)
+	body__sec66.fromTo('.aivsman-body__sec66 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+
+	ScrollTrigger.create({
+		animation: body__sec66,
+		trigger: '.aivsman-body__sec66',
+		scrub: 1,
+	})
+
+
+	const body__sec77 = gsap.timeline();
+
+	body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax1', 1, {y: 50}, {y:-50}, 0)
+	body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax2', 1, {y: 100}, {y:-100}, 0)
+	body__sec7.fromTo('.aivsman-body__sec77 .js-image-parallax3', 1, {y: -50}, {y:50}, 0)
+
+	ScrollTrigger.create({
+		animation: body__sec77,
+		trigger: '.aivsman-body__sec77',
+		scrub: 2,
+	})
 
 
 
@@ -365,7 +446,43 @@ ScrollTrigger.create({
 	} */
 
 
-	
+
+
+	$('.aivsman-body__subtitle.general-title').each(function() {
+		var text = $(this).text().split(' ');
+		var html = '';
+		for (var i = 0; i < text.length; i++) {
+			html += '<span class="title-outer">' + text[i] + '</span> ';
+		}
+		$(this).html(html);
+	});
+	$('.aivsman-body__subtitle.general-title .title-outer').each(function() {
+		var text = $(this).text().split(' ');
+		var html = '';
+		for (var i = 0; i < text.length; i++) {
+			html += '<span class="title-inner colored-text">' + text[i] + '</span> ';
+		}
+		$(this).html(html);
+	});
+
+
+
+	$('.forum__title.general-title').each(function() {
+		var text = $(this).text().split(' ');
+		var html = '';
+		for (var i = 0; i < text.length; i++) {
+			html += '<span class="title-outer">' + text[i] + '</span> ';
+		}
+		$(this).html(html);
+	});
+	$('.forum__title.general-title .title-outer').each(function() {
+		var text = $(this).text().split(' ');
+		var html = '';
+		for (var i = 0; i < text.length; i++) {
+			html += '<span class="title-inner">' + text[i] + '</span> ';
+		}
+		$(this).html(html);
+	});
 
 
 
